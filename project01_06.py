221bb351fecbb6fb51b9e82aaf22417c390d47d6
Ivanov Ivan <ivan@example.com>
2020-04-16 17:33:05 +0300

    Create project01_06.py
    My prjct first...

---
+++ project01_06.py
@@ -0,0 +1,19 @@
+print('Угадаю число от 1 до -')
+uppBrd = int(input())
+from random import random
+num = int(random() * uppBrd)
+print('Загадано число от 1 до', uppBrd, 'Это -', num)
+
+num1 = 0
+num2 = uppBrd
+count = 1
+mid = 0
+while num != mid:
+    count += 1
+    mid = int((num1 + num2) / 2)
+    if num > mid:
+        num1 = mid
+    elif num < mid:
+        num2 = mid
+
+print("Вы угадали число {num} за {count} попыток.")
