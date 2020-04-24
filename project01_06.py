728d369dcc17e373c6da25e748b74d3cb8689c9b
Ivanov Ivan <ivan@example.com>
2020-04-24 19:43:59 +0300

    Update project01_06.py

--- project01_06.py
+++ project01_06.py
@@ -1,6 +1,13 @@
 from random import random
+
+""""Угадывание числа. Методом бисекции или метод деления отрезка пополам."""
+
 print('Угадаю число от 1 до ... (введите верхнюю границу)')
-uppBrd = int(input())
+# uppBrd = int(input())
+
+"""Предполагалось что верхняя граница будет задаваться пользователем, но это оказалось излшним, поэтому фиксируем 100"""
+
+uppBrd = 100
 num = int(random() * uppBrd)
 print('Загадано число от 1 до', uppBrd, 'Это -', num)
 
@@ -10,10 +17,10 @@ count = 0
 mid = 0
 while num != mid:
     count += 1
-    mid = int((num1 + num2) / 2)
+    mid = (num1 + num2) // 2
     if num > mid:
         num1 = mid
     elif num < mid:
         num2 = mid
 
-print(f"Вы угадали число {num} за {count} попыток.")
+print(f"Вы угадали число {mid} за {count} попыток.")
